486d80adb372240439bb4134c91faf4f4815cbd8
Eran Rotem <eranrotem123@gmail.com>
2023-03-14 19:11:48 -0500

    set 4 and 5

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +8,72 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            return numbers.Where(x => x % 2 == 0).Sum() - numbers.Where(x => x % 2 != 0).Sum();
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            int[] lengths = { str1.Length, str2.Length, str3.Length, str4.Length};
+            return lengths.Min();
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            int[] nums = { number1, number2, number3, number4 };
+            return nums.Min();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            int[] sides = {sideLength1, sideLength2, sideLength3 };
+            int side1 = sides.Min();
+            int side3 = sides.Max();
+            int side2 = 1;
+            if (side1 == 0) { return false; }
+            foreach (var side in sides)
+            {
+                if (side != sides.Min() && side <= side3)
+                {
+                    side2 = side;
+                }
+            }
+
+            return Math.Pow(side1, 2) * Math.Pow(side2, 2) == Math.Pow(side3, 2);
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            return double.TryParse(input, out var num);
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            var nulls = objs.Where(x => x == null);
+            return nulls.Count() > objs.Length / 2;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            var nums = numbers.Where(x => x % 2 == 0);
+            var avg = nums.Average();
+            return (nums == null || !nums.Any()) ? 0 : avg;
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            if (number < 0)
+                throw new ArgumentOutOfRangeException();
+            int sum = 1 ;
+            for (int i = number; i > 0; i--)
+            {
+                sum *= i;
+            }
+            return sum;
         }
     }
 }
--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,37 +9,88 @@ namespace ChallengesWithTestsMark8
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
+            int x = startNumber;
+            if (x == 0) {return n; }
+            while (x % n != 0) 
+            {
+                x++;
+            }
+            if (x == startNumber) { return x += n; }
+            return x;
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
+            foreach (var bus in businesses)
+            {
+                if (bus.TotalRevenue == 0)
+                { bus.Name = "CLOSED"; }
+            }
         }
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0) {return false; }
+            var numsOrdered = numbers.OrderBy(x => x).ToArray();
+            return Enumerable.SequenceEqual(numbers, numsOrdered);
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
         {
-            throw new NotImplementedException();
+            var sum = 0;
+            if (numbers == null ) return sum;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    if (i + 1 <= numbers.Length - 1)
+                        sum += numbers[i + 1];
+                }
+            }
+            return sum;
         }
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            if (words == null || words.Length == 0) return "";
+            var strings = new List<string>();
+            foreach (var word in words)
+            {
+                if (string.IsNullOrWhiteSpace(word) == false)
+                {
+                    strings.Add(word.Trim());
+                }
+            }
+            if (string.IsNullOrWhiteSpace(string.Join("", strings))) { return ""; }
+            return string.Join(" ", strings) + ".";
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
         {
-            throw new NotImplementedException();
+            if (elements == null) return Array.Empty<double>();
+            var results = new List<double>();
+            for (int i = 3; i < elements.Count(); i += 4)
+            {
+                results.Add(elements[i]);
+            }
+            return results.ToArray();
         }
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
-        {
-            throw new NotImplementedException();
+        { 
+            if (nums.Length <= 1) { return false; }
+            var sums = new List<double>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        sums.Add(nums[i] + nums[j]);
+                    }
+                }
+            }
+            return sums.Contains(targetNumber);
         }
     }
 }
